f0a409842ec0d4d6fe7be1a5aa558a5c05608655
Ernesto Celis <ecelisdelafuente@pentalog.com>
2021-10-28 18:20:07 -0500

    Change privacy policy and footer copyright

--- pages/policy.js
+++ pages/policy.js
@@ -3,26 +3,22 @@ import Layout from '../components/layout'
 export default function PolicyPage () {
   return (
     <Layout>
-      <p>
-        This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
-      </p>
       <h2>Terms of Service</h2>
       <p>
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        SOFTWARE.
+        TODO.
       </p>
       <h2>Privacy Policy</h2>
       <p>
         This site uses JSON Web Tokens and an in-memory database which resets every ~2 hours.
       </p>
       <p>
-        Data provided to this site is exclusively used to support signing in
-        and is not passed to any third party services, other than via SMTP or OAuth for the
+        This site records personal data for archery athletes with the goal of registering them
+        into archery tournaments. The data is beign encrypted at storage and it is not shared
+        with people or organizations not related to registered achery clubs or organizations.
+      </p>
+      <p>
+        Data provided to this site with the purpose of supporting signing in
+        is not passed to any third party services, other than via SMTP or OAuth for the
         purposes of authentication.
       </p>
     </Layout>
