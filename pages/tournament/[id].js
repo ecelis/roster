b51e7179f134a24e47260aa38037ff8f1c802681
Ernesto Celis <ecelisdelafuente@pentalog.com>
2021-11-17 06:08:24 -0600

    Resolves ROS-3

--- pages/tournament/[id].js
+++ pages/tournament/[id].js
@@ -23,6 +23,9 @@ export default function RegistrationPage () {
             setTournament(data.find(e => {
                 return e._id === router.query.id
             }));
+            console.log('data', data)
+            console.log('setTournament', tournament)
+            
             sessionStorage.setItem('tournaments', JSON.stringify(data));
         }
     }, [router]);
@@ -32,12 +35,16 @@ export default function RegistrationPage () {
     return (
         <Layout>
             {(!session && tournament) && <>
-                <p>Sign in to register in {tournament.name} from {new Date(tournament.fromDate).toLocaleDateString()} to {new Date(tournament.toDate).toLocaleDateString()}</p>
+                <p>Sign in to register in {tournament.name} from
+                {' ' + new Date(tournament.fromDate).toLocaleDateString()} to
+                {' ' + new Date(tournament.toDate).toLocaleDateString()}</p>
             </>}
             {(session && tournament) &&
                 <Paper variant="outlined" sx={{ my: { xs: 3, md: 6}, p: { xs: 2, md: 3} }}>
                     <Typography component="h1" variant="h4" align="center">{`Registro ${tournament.name}`}</Typography>
-                    <Typography component="h6" variant="p" align="center">{`${new Date(tournament.fromDate).toLocaleDateString()} a ${new Date(tournament.toDate).toLocaleDateString()}`}</Typography>
+                    <Typography component="h6" variant="p" align="center">{
+                    `${new Date(tournament.fromDate).toLocaleDateString()} a ${new Date(tournament.toDate).toLocaleDateString()
+                    }`}</Typography>
                     <RegisterSteps tournament={tournament} />
                 </Paper>
             }
